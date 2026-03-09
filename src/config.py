bda7c86c52cc5be2f2c390869bab005d91b19994
Tyukhmenev Petr <Pvtyukhmenev@gmail.com>
2026-03-09 17:14:57 +0300

    add link for redis in config

--- src/config.py
+++ src/config.py
@@ -9,5 +9,14 @@ DB_NAME=os.environ.get('DB_NAME')
 DB_USER=os.environ.get('DB_USER')
 DB_PASS=os.environ.get('DB_PASS')
 
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+REDIS_DB = int(os.getenv("REDIS_DB"))
+
+REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
+
 SECRET=os.environ.get('SECRET')
+
 INACTIVE_LINK_DAYS = 30
--- src/core/redis.py
+++ src/core/redis.py
@@ -8,12 +8,14 @@ from fastapi_cache.backends.redis import RedisBackend
 
 from redis import asyncio as aioredis
 
+from config import REDIS_URL
+
 redis_client = None
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
     global redis_client
-    redis_client = aioredis.from_url("redis://localhost", decode_responses=True)
+    redis_client = aioredis.from_url(REDIS_URL, decode_responses=True)
     FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache")
     yield
     await redis_client.close()
